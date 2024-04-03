e50318108e7500f6fdc23254fb29890510a18ad5
Bagas Cahya  Fajar Bastian <bagascahyafajarbastian@gmail.com>
2024-04-03 21:39:38 +0700

    Update UI
    Update UI Web Using HTML Bootstrap. but responsive yet

---
+++ Tampilan Web/app/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  const $ = jQuery;
+  $('nav.navbar').addClass('navbar-active');
+  $(".navbar-nav .nav-link").hover(
+    function () {
+      $(this).css("color", "#007bff");
+    },
+    function () {
+      $(this).css("color", "#333");
+    }
+  );
+
+  $(".username-label").hover(
+    function () {
+      $(this).css("color", "#007bff");
+    },
+    function () {
+      $(this).css("color", "#333");
+    }
+  );
+});
